1175215a2d69d2a6a0617b89d812b635b9f62f65
Ian <ViViDboarder@gmail.com>
2023-11-24 22:26:06 -0800

    use info

--- rclone/rootfs/run_rclone.sh
+++ rclone/rootfs/run_rclone.sh
@@ -15,6 +15,6 @@ if bashio::config.true 'protected_only'; then
     FILTER="{\"IncludeRule\": $(find /backup -name "*.tar" -exec tar -xOf "{}" ./backup.json \;| jq -sc 'map(select(.protected) | "/backup/\(.slug).tar")')}"
 fi
 
-bashio::log.degug "Filter: $FILTER"
+bashio::log.info "Filter: $FILTER"
 
 rclone rc --user "$USERNAME" --pass "$PASSWORD" "sync/$SYNC_COMMAND" srcFs=/backup "dstFs=$DESTINATION" _async=true _filter="$FILTER"
