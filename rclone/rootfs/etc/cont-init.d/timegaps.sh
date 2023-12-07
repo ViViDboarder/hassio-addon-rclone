1a340d5829307db84214340707a1e4478f9de1e9
ViViDboarder <ViViDboarder@gmail.com>
2023-12-07 09:47:12 -0800

    Only run timegaps on included files

--- rclone/rootfs/etc/cont-init.d/timegaps.sh
+++ rclone/rootfs/etc/cont-init.d/timegaps.sh
@@ -1,16 +1,10 @@
 #! /usr/bin/with-contenv bashio
 
-bashio::log.info "Configuring pruning with timegaps..."
 TIMEGAPS_RULES=$(bashio::config 'prune.rules')
 TIMEGAPS_CRON=$(bashio::config 'prune.cron')
-TIMEGAPS_DELETE=""
-if bashio::config.true 'prune.delete'; then
-    TIMEGAPS_DELETE="--delete"
-fi
 
 if [ -n "$TIMEGAPS_RULES" ] && [ -n "$TIMEGAPS_CRON" ]; then
-    COMMAND="timegaps $TIMEGAPS_DELETE $TIMEGAPS_RULES /backup/*.tar"
-
-    echo "$TIMEGAPS_CRON $COMMAND" >> /etc/crontabs/root
+    bashio::log.info "Configuring pruning with timegaps..."
+    echo "$TIMEGAPS_CRON /run_timegaps.sh" >> /etc/crontabs/root
     crontab -l
 fi
---
+++ rclone/rootfs/run_timegaps.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/with-contenv bashio
+
+set +x
+set -eo pipefail
+
+bashio::log.info "Running timegaps..."
+
+TIMEGAPS_RULES=$(bashio::config 'prune.rules')
+TIMEGAPS_DELETE=""
+if bashio::config.true 'prune.delete'; then
+    TIMEGAPS_DELETE="--delete"
+fi
+
+if bashio::config.true 'protected_only'; then
+    find /backup -name "*.tar" -exec tar -xOf "{}" ./backup.json \; \
+        | jq -r 'select(.protected) | "\(.slug).tar"' \
+        | timegaps --stdin $TIMEGAPS_DELETE "$TIMEGAPS_RULES"
+else
+    timegaps $TIMEGAPS_DELETE "$TIMEGAPS_RULES"
+fi
+
